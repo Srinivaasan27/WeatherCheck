88facd981bba8d70a5390cb3f5043ac9f7e7d829
Aman Shetty <91794012+Srinivaasan27@users.noreply.github.com>
2022-02-06 12:41:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,89 @@
 const wrapper = document.querySelector(".wrapper"),
-inputPart = wrapper.querySelector(".input-part"),
+inputPart = document.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
-inputField = inputPart.querySelector("input");
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = wrapper.querySelector(".weather-part"),
+wIcon = weatherPart.querySelector("img"),
+arrowBack = wrapper.querySelector("header i");
 
-inputField.addEventListener("keyup", e=>{
+let api;
+
+inputField.addEventListener("keyup", e =>{
     if(e.key == "Enter" && inputField.value != ""){
         requestApi(inputField.value);
     }
 });
 
+locationBtn.addEventListener("click", () =>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert("Your browser not support geolocation api");
+    }
+});
+
 function requestApi(city){
-    let api = 'https://api.openweathermap.org/data/2.5/weather?q={city},India&appid=19a4bb6a569590e5a027476638637a1f';
-    fetch(api).then(response => console.log(response.json()));
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=19a4bb6a569590e5a027476638637a1f`;
+    fetchData();
 }
+
+function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=19a4bb6a569590e5a027476638637a1f`;
+    fetchData();
+}
+
+function onError(error){
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
+}
+
+function fetchData(){
+    infoTxt.innerText = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
+        infoTxt.innerText = "Something went wrong";
+        infoTxt.classList.replace("pending", "error");
+    });
+}
+
+function weatherDetails(info){
+    if(info.cod == "404"){
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+    }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {temp, feels_like, humidity} = info.main;
+
+        if(id == 800){
+            wIcon.src = "https://raw.githubusercontent.com/Srinivaasan27/WeatherCheck/main/clear.svg";
+        }else if(id >= 200 && id <= 232){
+            wIcon.src = "https://raw.githubusercontent.com/Srinivaasan27/WeatherCheck/main/storm.svg";  
+        }else if(id >= 600 && id <= 622){
+            wIcon.src = "https://raw.githubusercontent.com/Srinivaasan27/WeatherCheck/main/snow.svg";
+        }else if(id >= 701 && id <= 781){
+            wIcon.src = "https://raw.githubusercontent.com/Srinivaasan27/WeatherCheck/main/haze.svg";
+        }else if(id >= 801 && id <= 804){
+            wIcon.src = "https://raw.githubusercontent.com/Srinivaasan27/WeatherCheck/main/cloud.svg";
+        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
+            wIcon.src = "https://raw.githubusercontent.com/Srinivaasan27/WeatherCheck/main/rain.svg";
+        }
+        
+        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+        weatherPart.querySelector(".weather").innerText = description;
+        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+        infoTxt.classList.remove("pending", "error");
+        infoTxt.innerText = "";
+        inputField.value = "";
+        wrapper.classList.add("active");
+    }
+}
+
+arrowBack.addEventListener("click", ()=>{
+    wrapper.classList.remove("active");
+});
