969c0bc906a2e5354ef8a7df1fa585fd0ed885f0
Aman Shetty <91794012+Srinivaasan27@users.noreply.github.com>
2022-02-06 11:28:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const wrapper = document.querySelector(".wrapper")
+const wrapper = document.querySelector(".wrapper"),
+inputPart = wrapper.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input");
+
+inputField.addEventListener("keyup", e=>{
+    if(e.key == "Enter" && inputField.value != ""){
+        requestApi(inputField.value);
+    }
+});
+
+function requestApi(city){
+    let api = 'https://api.openweathermap.org/data/2.5/weather?q={city},India&appid=19a4bb6a569590e5a027476638637a1f';
+    fetch(api).then(response => console.log(response.json()));
+}
